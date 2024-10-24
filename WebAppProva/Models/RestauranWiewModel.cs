8cbd49f8165e10d0dd20c771e508a5ea9dbbb2d1
Iurii <yurakaramyshev@yandex.com>
2024-10-24 19:51:58 +0200

    Delete git commits

--- WebAppProva/Models/RestauranWiewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebAppProva.Models
-{
-    public class RestauranWiewModel
-    {
-        public string OrderName { get; set; } 
-        public double Price {  get; set; } 
-        public double? OldPrice { get; set; }
-    }
-}
